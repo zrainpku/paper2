b5e593cef4d89546c740550fc0a03054b60b76a3
朱瑞 <zhurui_i@didichuxing.com>
2019-04-22 10:57:31 +0800

    fix frangi.cpp

--- frangi.cpp
+++ frangi.cpp
@@ -9,7 +9,7 @@
 #include <stdio.h>
 #include "hash.hpp"
 
-void frangi2d_hessian(const Mat &src, Mat &Dxx, Mat &Dxy, Mat &Dyy, float sigma){
+void frangi2d_hessian(const cv::Mat &src, cv::Mat &Dxx, cv::Mat &Dxy, cv::Mat &Dyy, float sigma){
     //construct Hessian kernels
     int n_kern_x = 2*round(3*sigma) + 1;
     int n_kern_y = n_kern_x;
@@ -53,22 +53,22 @@ void frangi2d_hessian(const Mat &src, Mat &Dxx, Mat &Dxy, Mat &Dyy, float sigma)
     }
     
     //flip kernels since kernels aren't symmetric and opencv's filter2D operation performs a correlation, not a convolution
-    Mat kern_xx;
-    flip(Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xx_f), kern_xx, -1);
+    cv::Mat kern_xx;
+    flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xx_f), kern_xx, -1);
     
-    Mat kern_xy;
-    flip(Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xy_f), kern_xy, -1);
+    cv::Mat kern_xy;
+    flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_xy_f), kern_xy, -1);
     
-    Mat kern_yy;
-    flip(Mat(n_kern_y, n_kern_x, CV_32FC1, kern_yy_f), kern_yy, -1);
+    cv::Mat kern_yy;
+    flip(cv::Mat(n_kern_y, n_kern_x, CV_32FC1, kern_yy_f), kern_yy, -1);
     
     //specify anchor since we are to perform a convolution, not a correlation
-    Point anchor(n_kern_x - n_kern_x/2 - 1, n_kern_y - n_kern_y/2 - 1);
+    cv::Point anchor(n_kern_x - n_kern_x/2 - 1, n_kern_y - n_kern_y/2 - 1);
     
     //run image filter
-    filter2D(src, Dxx, -1, kern_xx, anchor);
-    filter2D(src, Dxy, -1, kern_xy, anchor);
-    filter2D(src, Dyy, -1, kern_yy, anchor);
+    cv::filter2D(src, Dxx, -1, kern_xx, anchor);
+    cv::filter2D(src, Dxy, -1, kern_xy, anchor);
+    cv::filter2D(src, Dyy, -1, kern_yy, anchor);
     
     
     delete [] kern_xx_f;
@@ -88,34 +88,34 @@ void frangi2d_createopts(frangi2d_opts_t *opts){
     opts->BlackWhite = DEFAULT_BLACKWHITE;
 }
 
-void frangi2_eig2image(const Mat &Dxx, const Mat &Dxy, const Mat &Dyy, Mat &lambda1, Mat &lambda2, Mat &Ix, Mat &Iy){
+void frangi2_eig2image(const cv::Mat &Dxx, const cv::Mat &Dxy, const cv::Mat &Dyy, cv::Mat &lambda1, cv::Mat &lambda2, cv::Mat &Ix, cv::Mat &Iy){
     //calculate eigenvectors of J, v1 and v2
-    Mat tmp, tmp2;
+    cv::Mat tmp, tmp2;
     tmp2 = Dxx - Dyy;
     sqrt(tmp2.mul(tmp2) + 4*Dxy.mul(Dxy), tmp);
-    Mat v2x = 2*Dxy;
-    Mat v2y = Dyy - Dxx + tmp;
+    cv::Mat v2x = 2*Dxy;
+    cv::Mat v2y = Dyy - Dxx + tmp;
     
     //normalize
-    Mat mag;
+    cv::Mat mag;
     sqrt((v2x.mul(v2x) + v2y.mul(v2y)), mag);
-    Mat v2xtmp = v2x.mul(1.0f/mag);
+    cv::Mat v2xtmp = v2x.mul(1.0f/mag);
     v2xtmp.copyTo(v2x, mag != 0);
-    Mat v2ytmp = v2y.mul(1.0f/mag);
+    cv::Mat v2ytmp = v2y.mul(1.0f/mag);
     v2ytmp.copyTo(v2y, mag != 0);
     
     //eigenvectors are orthogonal
-    Mat v1x, v1y;
+    cv::Mat v1x, v1y;
     v2y.copyTo(v1x);
     v1x = -1*v1x;
     v2x.copyTo(v1y);
     
     //compute eigenvalues
-    Mat mu1 = 0.5*(Dxx + Dyy + tmp);
-    Mat mu2 = 0.5*(Dxx + Dyy - tmp);
+    cv::Mat mu1 = 0.5*(Dxx + Dyy + tmp);
+    cv::Mat mu2 = 0.5*(Dxx + Dyy - tmp);
     
     //sort eigenvalues by absolute value abs(Lambda1) < abs(Lamda2)
-    Mat check = abs(mu1) > abs(mu2);
+    cv::Mat check = abs(mu1) > abs(mu2);
     mu1.copyTo(lambda1); mu2.copyTo(lambda1, check);
     mu2.copyTo(lambda2); mu1.copyTo(lambda2, check);
     for(int i=0;i<lambda2.rows;i++)
@@ -134,15 +134,15 @@ void frangi2_eig2image(const Mat &Dxx, const Mat &Dxy, const Mat &Dyy, Mat &lamb
 }
 
 
-void frangi2d(const Mat &src, Mat &maxVals, Mat &whatScale, Mat &outAngles, frangi2d_opts_t opts){
-    vector<Mat> ALLfiltered;
-    vector<Mat> ALLangles;
+void frangi2d(const cv::Mat &src, cv::Mat &maxVals, cv::Mat &whatScale, cv::Mat &outAngles, frangi2d_opts_t opts){
+    std::vector<cv::Mat> ALLfiltered;
+    std::vector<cv::Mat> ALLangles;
     float beta = 2*opts.BetaOne*opts.BetaOne;
     float c = 2*opts.BetaTwo*opts.BetaTwo;
     
     for (float sigma = opts.sigma_start; sigma <= opts.sigma_end; sigma += opts.sigma_step){
         //create 2D hessians
-        Mat Dxx, Dyy, Dxy;
+        cv::Mat Dxx, Dyy, Dxy;
         frangi2d_hessian(src, Dxx, Dxy, Dyy, sigma);
         
         //correct for scale
@@ -151,27 +151,27 @@ void frangi2d(const Mat &src, Mat &maxVals, Mat &whatScale, Mat &outAngles, fran
         Dxy = Dxy*sigma*sigma;
         
         //calculate (abs sorted) eigenvalues and vectors
-        Mat lambda1, lambda2, Ix, Iy;
+        cv::Mat lambda1, lambda2, Ix, Iy;
         frangi2_eig2image(Dxx, Dxy, Dyy, lambda1, lambda2, Ix, Iy);
         
         //compute direction of the minor eigenvector
-        Mat angles;
+        cv::Mat angles;
         phase(Ix, Iy, angles);
         ALLangles.push_back(angles);
         
         //compute some similarity measures
         //cout<<"lambda2type:"<<lambda2.type()<<endl;
         //lambda2.setTo(nextafterf(0, 1), lambda2==0);
-        Mat Rb = lambda1.mul(1.0/lambda2);
+        cv::Mat Rb = lambda1.mul(1.0/lambda2);
         Rb = Rb.mul(Rb);
-        Mat S2 = lambda1.mul(lambda1) + lambda2.mul(lambda2);
+        cv::Mat S2 = lambda1.mul(lambda1) + lambda2.mul(lambda2);
         
         //compute output image
-        Mat tmp1, tmp2;
+        cv::Mat tmp1, tmp2;
         exp(-Rb/beta, tmp1);
         exp(-S2/c, tmp2);
         
-        Mat Ifiltered = tmp1.mul(Mat::ones(src.rows, src.cols, src.type()) - tmp2);
+        cv::Mat Ifiltered = tmp1.mul(cv::Mat::ones(src.rows, src.cols, src.type()) - tmp2);
         if (opts.BlackWhite){
             //zr
             Ifiltered.setTo(0, lambda2 > 0);
@@ -204,15 +204,15 @@ void frangi2d(const Mat &src, Mat &maxVals, Mat &whatScale, Mat &outAngles, fran
 
 
 
-void pore_2d(const Mat &src, Mat &maxVals, Mat &whatScale, Mat &outAngles, frangi2d_opts_t opts){
-    vector<Mat> ALLfiltered;
-    vector<Mat> ALLangles;
+void pore_2d(const cv::Mat &src, cv::Mat &maxVals, cv::Mat &whatScale, cv::Mat &outAngles, frangi2d_opts_t opts){
+    std::vector<cv::Mat> ALLfiltered;
+    std::vector<cv::Mat> ALLangles;
     float beta = 2*opts.BetaOne*opts.BetaOne;
     float c = 2*opts.BetaTwo*opts.BetaTwo;
     
     for (float sigma = opts.sigma_start; sigma <= opts.sigma_end; sigma += opts.sigma_step){
         //create 2D hessians
-        Mat Dxx, Dyy, Dxy;
+        cv::Mat Dxx, Dyy, Dxy;
         frangi2d_hessian(src, Dxx, Dxy, Dyy, sigma);
         
         //correct for scale
@@ -221,27 +221,27 @@ void pore_2d(const Mat &src, Mat &maxVals, Mat &whatScale, Mat &outAngles, frang
         Dxy = Dxy*sigma*sigma;
         
         //calculate (abs sorted) eigenvalues and vectors
-        Mat lambda1, lambda2, Ix, Iy;
+        cv::Mat lambda1, lambda2, Ix, Iy;
         frangi2_eig2image(Dxx, Dxy, Dyy, lambda1, lambda2, Ix, Iy);
         
         //compute direction of the minor eigenvector
-        Mat angles;
+        cv::Mat angles;
         phase(Ix, Iy, angles);
         ALLangles.push_back(angles);
         
         //compute some similarity measures
         //cout<<"lambda2type:"<<lambda2.type()<<endl;
         //lambda2.setTo(nextafterf(0, 1), lambda2==0);
-        Mat Rb = lambda1.mul(1.0/lambda2);
+        cv::Mat Rb = lambda1.mul(1.0/lambda2);
         Rb = Rb.mul(Rb);
-        Mat S2 = lambda1.mul(lambda1) + lambda2.mul(lambda2);
+        cv::Mat S2 = lambda1.mul(lambda1) + lambda2.mul(lambda2);
         
         //compute output image
-        Mat tmp1, tmp2;
+        cv::Mat tmp1, tmp2;
         exp(-Rb/beta, tmp1);
         exp(-S2/c, tmp2);
         
-        Mat Ifiltered = tmp1.mul(Mat::ones(src.rows, src.cols, src.type()) - tmp2);
+        cv::Mat Ifiltered = tmp1.mul(cv::Mat::ones(src.rows, src.cols, src.type()) - tmp2);
         if (opts.BlackWhite){
             //zr
             /*  for(int i=0;i<lambda2.rows;i++)
